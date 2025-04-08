badf77987fdb7c5b42cb5d3265e8d3f65b28d126
mykola.bolshakov <mykola.bolshakov@gmail.com>
2025-04-08 05:35:35 +0300

    MAIN | Add theme.

--- webroot/sites/default/settings.php
+++ webroot/sites/default/settings.php
@@ -874,6 +874,11 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
  *
  * Keep this code block at the end of this file to take full effect.
  */
+
+if (empty($settings['config_sync_directory'])) {
+  $settings['config_sync_directory'] = '../configs/sync/default';
+}
+
 #
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
